98cb0d7286134b12ccd8dac0b8d4a815f610e57f
Allyson Villaflor <132638899+villafla@users.noreply.github.com>
2025-03-02 08:32:40 +0000

    Added comments and headers

--- signals.h
+++ signals.h
@@ -1,3 +1,21 @@
+/*
+* Program Name: Programming Assignment 4: SMALLSH
+* Author: Allyson Villaflor
+* Email: villafla@oregonstate.edu
+* CS 374 - Operating Systems I
+* Program description: This program creates a shell called smallsh. smallsh implements a subset
+*                      if well-known shells, such as bash. The program does the following:
+*          
+*                      - Provides a prompt for running commands
+*                      - Handles blank lines and comments, which are lines beginning with the # character
+*                      - Executes 3 commands exit, cd, and status via code built into the shell
+*                      - Executes other commands by creating new processes using a function from 
+*                        the exec() family of functions
+*                      - Supports input and output redirection
+*                      - Supports running commands in foregrounf and background processes
+*                      - Implements custom handlers for 2 signals, SIGINT SIGTSTP
+*/
+
 #ifndef SIGNALS_H
 #define SIGNALS_H
 
