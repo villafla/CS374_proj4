eec0e3839ce21a5624e6ad904937267d3985190e
Allyson Villaflor <132638899+villafla@users.noreply.github.com>
2025-03-02 08:42:53 +0000

    Added comments and headers

--- parser.h
+++ parser.h
@@ -1,9 +1,30 @@
+/*
+* Program Name: Programming Assignment 4: SMALLSH
+* Author: Allyson Villaflor
+* Email: villafla@oregonstate.edu
+* CS 374 - Operating Systems I
+* Program description: This program creates a shell called smallsh. smallsh implements a subset
+*                      if well-known shells, such as bash. The program does the following:
+*          
+*                      - Provides a prompt for running commands
+*                      - Handles blank lines and comments, which are lines beginning with the # character
+*                      - Executes 3 commands exit, cd, and status via code built into the shell
+*                      - Executes other commands by creating new processes using a function from 
+*                        the exec() family of functions
+*                      - Supports input and output redirection
+*                      - Supports running commands in foregrounf and background processes
+*                      - Implements custom handlers for 2 signals, SIGINT SIGTSTP
+*/
+
+/*
+* Citation: The following function adapts code from sample_parser.c
+* 
+*/
+
 #ifndef PARSER_H
 #define PARSER_H
 
 #include "smallsh.h"
-
-// The following code implements the sample parser 
 
 struct command_line *parse_input();
 
